13b1a3263c3612b145c144b5bb6742685285b720
chunguyenduc <nguyenducchu1999@gmail.com>
2021-05-22 19:18:59 +0700

    add GUI

---
+++ gui.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.geometry("600x600")
+    root.title('Speaker Verification System')
+    B = tk.Button(root, text="Hello")
+    B.pack()
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
